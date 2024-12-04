6bb04769c5272777978fe7cdadc11738d83f3e3e
Ilya Ivanov <ivn25ov@mail.ru>
2024-12-04 18:16:21 +0400

    save progress

--- social-network/auth-service/src/main/java/dev/ivanov/social_network/auth_service/controllers/AuthController.java
+++ social-network/auth-service/src/main/java/dev/ivanov/social_network/auth_service/controllers/AuthController.java
@@ -1,16 +1,30 @@
 package dev.ivanov.social_network.auth_service.controllers;
 
-import dev.ivanov.social_network.auth_service.dto.ChangePasswordDto;
-import dev.ivanov.social_network.auth_service.dto.RefreshDto;
-import dev.ivanov.social_network.auth_service.dto.SignInDto;
-import dev.ivanov.social_network.auth_service.dto.SignUpDto;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import dev.ivanov.social_network.auth_service.dto.*;
+import dev.ivanov.social_network.auth_service.entities.Account;
+import dev.ivanov.social_network.auth_service.exceptions.AccountNotFoundException;
+import dev.ivanov.social_network.auth_service.exceptions.ActionWithDeletedAccountException;
+import dev.ivanov.social_network.auth_service.exceptions.RemoteServiceException;
+import dev.ivanov.social_network.auth_service.producers.AccountEventsProducer;
+import dev.ivanov.social_network.auth_service.security.UserDetailsImpl;
 import dev.ivanov.social_network.auth_service.services.AuthService;
 import dev.ivanov.social_network.auth_service.validators.ChangePasswordDtoValidator;
 import dev.ivanov.social_network.auth_service.validators.SignUpDtoValidator;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
@@ -23,24 +37,77 @@ public class AuthController {
     @Autowired
     private ChangePasswordDtoValidator changePasswordDtoValidator;
 
+    @Autowired
+    private AccountEventsProducer accountEventsProducer;
+
     @PostMapping("/sign-in")
     public ResponseEntity<?> signIn(@RequestBody SignInDto signInDto) {
-        return null;
+        try {
+            JwtDto jwtDto = authService.signIn(signInDto);
+            return ResponseEntity.ok(jwtDto);
+
+        } catch (AccountNotFoundException | UsernameNotFoundException e) {
+
+            log.error(e.getMessage());
+            return ResponseEntity.notFound().build();
+
+        } catch (AuthenticationException e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
     }
 
     @PostMapping("/sign-up")
     public ResponseEntity<?> signUp(@RequestBody SignUpDto signUpDto) {
-        return null;
+
+        try {
+            Errors errors = new BeanPropertyBindingResult(signUpDto, "signUpDto");
+            signUpDtoValidator.validate(signUpDto, errors);
+
+            if (errors.hasErrors())
+                return ResponseEntity.badRequest().body(errors.getAllErrors()
+                        .stream()
+                        .map(DefaultMessageSourceResolvable::getDefaultMessage));
+
+            JwtDto jwtDto = authService.signUp(signUpDto);
+
+            return ResponseEntity.ok(jwtDto);
+        } catch (RemoteServiceException e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
     }
 
     @PostMapping("/refresh")
     public ResponseEntity<?> refresh(@RequestBody RefreshDto refreshDto) {
-        return null;
+        try {
+            JwtDto jwtDto = authService.refresh(refreshDto);
+            return ResponseEntity.ok(jwtDto);
+        } catch (JWTVerificationException | ActionWithDeletedAccountException |  e) {
+            log.error(e.getMessage());
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @PutMapping("/change-password")
     public ResponseEntity<?> changePassword(@RequestBody ChangePasswordDto changePasswordDto) {
-        return null;
+        try {
+            Errors errors = new BeanPropertyBindingResult(changePasswordDto, "changePasswordDto");
+            changePasswordDtoValidator.validate(changePasswordDto, errors);
+
+            if (errors.hasErrors()) {
+                return ResponseEntity.badRequest().body(errors.getAllErrors()
+                        .stream()
+                        .map(DefaultMessageSourceResolvable::getDefaultMessage));
+            }
+
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+            Account account = userDetails.getAccount();
+
+            authService.changePassword(account.getId(), changePasswordDto);
+        } catch ()
     }
 
     @DeleteMapping("/delete-account/{accountId}")
--- social-network/auth-service/src/main/java/dev/ivanov/social_network/auth_service/security/JwtFilter.java
+++ social-network/auth-service/src/main/java/dev/ivanov/social_network/auth_service/security/JwtFilter.java
@@ -1,4 +1,27 @@
 package dev.ivanov.social_network.auth_service.security;
 
-public class JwtFilter {
+import dev.ivanov.social_network.auth_service.services.JwtUtils;
+import jakarta.annotation.Nonnull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Override
+    protected void doFilterInternal(@Nonnull HttpServletRequest request,
+                                    @Nonnull HttpServletResponse response,
+                                    @Nonnull FilterChain filterChain) throws ServletException, IOException {
+
+    }
 }
